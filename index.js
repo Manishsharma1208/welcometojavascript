e810381a5d9cd11a59c5c3c187e74266eefc9e31
Manishsharma1208 <brahmanmanish1208@gmail.com>
2022-05-28 00:06:40 +0530

    Intro tojavascript

---
+++ index.js
@@ -0,0 +1,20 @@
+//var let const
+//typeof a 
+var a=20;
+console.log(a);
+console.log(typeof a);  //type of operator is used for type of datadtored in variable
+
+a ="hello World";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+var b = `hello everyone hope you all are good`;
+console.log(b);
+
+//use of \n to add content in one line
+var num = 4500;
+console.log(`half of ${num}is half ${num/2} is`);
